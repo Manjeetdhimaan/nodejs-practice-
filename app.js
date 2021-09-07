a4db1c97c4c79ec4319b0667e007351d6ee1b608
Manjeetdhimaan <80199360+Manjeetdhimaan@users.noreply.github.com>
2021-09-07 10:47:21 +0530

    created login form

--- app.js
+++ app.js
@@ -1,30 +1,72 @@
 const express = require('express');
+
 const app = express();
 
-const router = express.Router();
-
-const checkUrl = require('./middleware')
+var bodyParser = require('body-parser');
+var encoder = bodyParser.urlencoded();
+app.use('/assests', express.static('assests'))
+app.set('view engine', 'ejs')
+app.get('/profile/:name', function(req, res){
+    data={email:'test@test.com', address:'mohali', skills:['javascript', 'angular', 'git', 'react']}
+   res.render('Profile', {name:req.params.name, data:data})
+   console.log(req.params.name)
+})
 
 app.get('/', function(req, res){
-    // res.send("Hello this is home page")
-    res.sendFile(__dirname+"/Home.html")
-    });
-router.get('/about', checkUrl, function(req, res){
-// res.send("Hello this is about page")
-res.sendFile(__dirname+"/About.html")
+res.render('Home')
+})
+app.post('/login',encoder,  function(req, res){
+    console.log(req.body.email)
+    res.render('home')
+    })
 
-});
 app.get('/login', function(req, res){
-// res.send("Hello this is login page")
-res.sendFile(__dirname+"/Login.html")
-
-});
-
-
-app.use('/', router)
+res.render('login')
+})
 app.listen(3000, function () {
     console.log("Express server listening on port 3000");
-    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+
+// const router = express.Router();
+
+// const checkUrl = require('./middleware')
+
+// app.get('/', function(req, res){
+//     // res.send("Hello this is home page")
+//     res.sendFile(__dirname+"/Home.html")
+//     });
+// router.get('/about', checkUrl, function(req, res){
+// // res.send("Hello this is about page")
+// res.sendFile(__dirname+"/About.html")
+
+// });
+// app.get('/login', function(req, res){
+// // res.send("Hello this is login page")
+// res.sendFile(__dirname+"/Login.html")
+
+// });
+
+
+// app.use('/', router)
+// app.listen(3000, function () {
+//     console.log("Express server listening on port 3000");
+//     });
   
     
 
